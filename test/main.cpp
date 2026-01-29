0a729ff3fabad2832d9b07fbb0eefacad632309c
DivanadiumPentaoxide <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2026-01-29 20:53:00 +0800

    chore: update test

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,7 @@
 #include <list>
 #include <set>
 
-using namespace jsonc::reflection;
+namespace jr = jsonc::reflection;
 
 struct TestType1 {
     TestType1() = default;
@@ -34,26 +34,26 @@ enum class TestEnum { AAA = 0, BBB = 1, CCC = 2, DDD = 3 };
 enum class TestEnumFlag { AAA = 0, BBB = 1 << 0, CCC = 1 << 1, DDD = 1 << 2, EEE = BBB | DDD };
 
 struct Config {
-    std::string                                                 test_1  = "test string";
-    Annotated<std::string, "xiwhgasdjjhoikwq">                  test_2  = "test string with comments";
-    Renamed<Annotated<std::string, "738whdbhahisdS">, "test-3"> test_3  = "test string with renamed key and comments";
-    int                                                         test_4  = 232123;
-    double                                                      test_5  = 3.526781;
-    float                                                       test_6  = 26781.234f;
-    int16_t                                                     test_7  = -2671;
-    std::optional<uint8_t>                                      test_8  = {123};
-    std::string                                                 test_9  = "str test";
-    TestType1                                                   test_10 = {123.456f};
-    Ranged<short, -3, 5678>                                     test_11 = 23345;
-    TestEnum                                                    test_12 = TestEnum(3);
-    TestEnumFlag                                                test_13 = TestEnumFlag(6);
-    std::variant<bool, float, int, uint64_t>                    test_14 = 2.345f;
-    std::tuple<int, int, std::string, float, TestEnum>          test_15 = {234, -3782, "wyuhdsj", 7.492f, TestEnum::BBB};
-    std::list<Annotated<int, "xwsaijnjhiknjk">>                 test_16 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::vector<std::string>                                    test_17 = {"xxxxx", "yyyyy"};
-    std::array<short, 4>                                        test_18 = {22, 33, 44, 55};
-    bool                                                        test_19 = false;
-    std::unordered_map<int16_t, float>                          test_20 = {
+    std::string                                                         test_1  = "test string";
+    jr::Annotated<std::string, "xiwhgasdjjhoikwq">                      test_2  = "test string with comments";
+    jr::Renamed<jr::Annotated<std::string, "738whdbhahisdS">, "test-3"> test_3  = "test string with renamed key and comments";
+    int                                                                 test_4  = 232123;
+    double                                                              test_5  = 3.526781;
+    float                                                               test_6  = 26781.234f;
+    int16_t                                                             test_7  = -2671;
+    std::optional<uint8_t>                                              test_8  = {123};
+    std::string                                                         test_9  = "str test";
+    TestType1                                                           test_10 = {123.456f};
+    jr::Ranged<short, -3, 5678>                                         test_11 = 23345;
+    TestEnum                                                            test_12 = TestEnum(3);
+    TestEnumFlag                                                        test_13 = TestEnumFlag(6);
+    std::variant<bool, float, int, uint64_t>                            test_14 = 2.345f;
+    std::tuple<int, int, std::string, float, TestEnum>                  test_15 = {234, -3782, "wyuhdsj", 7.492f, TestEnum::BBB};
+    std::list<jr::Annotated<int, "xwsaijnjhiknjk">>                     test_16 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<std::string>                                            test_17 = {"xxxxx", "yyyyy"};
+    std::array<short, 4>                                                test_18 = {22, 33, 44, 55};
+    bool                                                                test_19 = false;
+    std::unordered_map<int16_t, float>                                  test_20 = {
         {{1, 2.3f}, {4, 5.32f}, {23, 2.45f}}
     };
     std::map<TestEnum, std::variant<bool, int, std::string>> test_21 = {
@@ -61,15 +61,15 @@ struct Config {
     };
     TestType2 test_22 = {"123456787654323456789824324543454323456765432345676543765432345678"};
     struct Test24 {
-        int                         xxxxx = 123;
-        Annotated<double, "double"> yyyyy = 64738.543;
+        int                             xxxxx = 123;
+        jr::Annotated<double, "double"> yyyyy = 64738.543;
     } test_23;
     std::set<int>              test_24 = {1, 23, 456};
     std::vector<jsonc::Object> test_25{};
 };
 
 int main() {
-    Annotated<std::variant<Config, std::vector<int>>, "test config", "xxxxx"> settings;
-    jsonc::reflection::load_file(settings, "./test.jsonc", {});
+    jr::Annotated<std::variant<Config, std::vector<int>>, "test config", "xxxxx"> settings;
+    jr::load_file(settings, "./test.jsonc");
     return 0;
 }
