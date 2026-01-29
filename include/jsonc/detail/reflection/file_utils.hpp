edc3c6cba28960330e9620c8b31d1f4a2bf99f36
DivanadiumPentaoxide <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2026-01-30 01:23:46 +0800

    fix: add missing inline

--- include/jsonc/detail/reflection/file_utils.hpp
+++ include/jsonc/detail/reflection/file_utils.hpp
@@ -5,13 +5,13 @@
 
 namespace jsonc::reflection::file_utils {
 
-std::optional<std::string> read_file(std::filesystem::path const& path) noexcept {
+inline std::optional<std::string> read_file(std::filesystem::path const& path) noexcept {
     if (!std::filesystem::exists(path)) { return std::nullopt; }
     auto file = std::ifstream(path, std::ios_base::in);
     return std::string(std::istreambuf_iterator<char>(file), {});
 }
 
-bool write_file(std::filesystem::path const& path, std::string_view content) noexcept {
+inline bool write_file(std::filesystem::path const& path, std::string_view content) noexcept {
     if (!std::filesystem::exists(path.parent_path())) { std::filesystem::create_directories(path.parent_path()); }
     auto file = std::ofstream(path, std::ios_base::out);
     file << content;
